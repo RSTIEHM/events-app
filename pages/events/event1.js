399c2c3c1e4cc7d24ab171229c943a003391f11d
Richard Stiehm <buzzysanders@gmail.com>
2022-12-26 18:20:20 -0500

    Basic Pages Created

---
+++ pages/events/event1.js
@@ -0,0 +1,9 @@
+const Page = () => {
+    return(
+        <div>
+            <h1>Event Page</h1>
+        </div>
+    )
+}
+
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,26 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>All Events Page</h1>
+            <div>
+                <a href="">
+                    <img />
+                    <h2>Events In London</h2>
+
+                </a>
+                <a href="">
+                    <img />
+                    <h2>Events In San Francisco</h2>
+
+                </a>
+                <a href="">
+                    <img />
+                    <h2>Events In New York</h2>
+
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Page;
